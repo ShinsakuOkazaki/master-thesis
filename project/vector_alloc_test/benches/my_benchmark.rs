fe4de349595d02125ea6247eb722fe2ce8fe23b9
sinsakuokazaki <golisaku4639@gmail.com>
2019-11-24 19:22:57 -0500

    changed format

--- project/vector_alloc_test/benches/my_benchmark.rs
+++ project/vector_alloc_test/benches/my_benchmark.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use vector_alloc_test::vector_addition;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("intial: 10, fanal 9", |b| b.iter(|| vector_addition(black_box(10), black_box(9))));
+    c.bench_function("intial_10_fanal_9", |b| b.iter(|| vector_addition(black_box(10), black_box(9))));
     c.bench_function("intial_100_fanal_9", |b| b.iter(|| vector_addition(black_box(100), black_box(9))));
     c.bench_function("intial_1000_fanal_9", |b| b.iter(|| vector_addition(black_box(1000), black_box(9))));
     c.bench_function("intial_10000_fanal_9", |b| b.iter(|| vector_addition(black_box(10000), black_box(9))));

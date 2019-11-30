48908e7f744d6d16fde873bfd0e4a86524209142
sinsakuokazaki <golisaku4639@gmail.com>
2019-11-30 14:43:20 -0500

    resolved run.sh

--- project/javamemoryallocmanual/run.sh
+++ project/javamemoryallocmanual/run.sh
@@ -6,8 +6,9 @@ mvn clean compile assembly:single
 for size in 1 2 3 4
 do
 
-  echo "datastructure#initialization#size#inittime#addtime#totaltime" > loging.log
+
   rm -rf loging.log
+  echo "datastructure#initialization#size#inittime#addtime#totaltime" > loging.log
 
 	for counter in 1 2 3 4 5
 	do
@@ -22,12 +23,16 @@ do
 
 	    echo "Adding $size elements with initialization $initialization in Run number  $counter "
 
+      # Server experiment
 	    time taskset -c 0 java -Xms1g -Xmx5g   -cp  ./target/java-memory-alloc-manual-1.0-SNAPSHOT-jar-with-dependencies.jar  allocation/ElementsAddition   $size $initialization
+
+	    # Loacal experiment
+	    # time java -Xms1g -Xmx5g   -cp  ./target/java-memory-alloc-manual-1.0-SNAPSHOT-jar-with-dependencies.jar  allocation/ElementsAddition   $size $initialization
 
 
     done
 
-    cat loging.log  >>  result/resultJavaAlloc_"$size".txt
-	done
 
+	done
+  cat loging.log  >  result/resultJavaAlloc_"$size".txt
 done

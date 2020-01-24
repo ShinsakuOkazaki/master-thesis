d80474857197833e409d5158dbd6ee2c126eb2f4
sinsakuokazaki <golisaku4639@gmail.com>
2020-01-23 21:48:17 -0500

    experimented

--- project/playground/src/main.rs
+++ project/playground/src/main.rs
@@ -8,6 +8,7 @@ use std::mem;
 
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
+use rand::distributions::{Distribution, Uniform};
 
 fn main() {
 
@@ -27,31 +28,76 @@ fn add_elements(size: usize, method: i32) {
     let mut distination = Vec::with_capacity(size);
     let elapsed_init = start_init.elapsed().as_nanos();
 
-    
+    let dist = Uniform::from(3..7);
     let mut source = Vec::with_capacity(size);
     for _i in 0..size {
         let rand_string: String = thread_rng()
                             .sample_iter(&Alphanumeric)
-                            .take(10)
+                            .take(dist.sample(&mut thread_rng()))
                             .collect();
         source.push(rand_string);    
     }
 
     // Make condition when clone is used and otherwise.
-    let elapsed_add = unsafe {select_experiment(method, &mut distination, &mut source, size)};
-    
+    let elapsed_add = do_clone_from(&mut distination, &mut source);
+
+    let immutable = distination;
+
+    let elapsed_access = access_string_test(&immutable, &source);
     
     let elapsed_total = start_init.elapsed().as_nanos();
-
-    println!("Before source {:?}", source[5]);
-    println!("Before distination {:?}", distination[5]);
-    distination[5].push_str("shin");
-    println!("After source{:?}", source[5]);
-    println!("After distination {:?}", distination[5]);
-
-    let output = format!("[RustVector]#{:?}#{:?}#{:?}#{:?}\n", size, elapsed_init, elapsed_add, elapsed_total);
+    
+    let output = format!("[RustVector]#{:?}#{:?}#{:?}#{:?}#{:?}\n", size, elapsed_init, elapsed_add, elapsed_access ,elapsed_total);
 
     println!("{}",output);
+}
+
+fn access_integer_test(immutable :&Vec<i32>, source: &Vec<i32>) -> u128 {
+    let start_access = Instant::now();
+    let len = immutable.len();
+    let mut sum: i64 = 0;
+    for i in 0..len {
+        sum += (immutable[i] as i64);
+    }
+    let elapsed_access = start_access.elapsed().as_nanos();
+
+    let mut true_sum: i64 = 0;
+    for i in 0..len {
+        true_sum += (i as i64);
+    }
+    let correct = (sum == true_sum);
+    println!("{}", correct);
+    return elapsed_access;
+}
+
+fn access_string_test(immutable :&Vec<String>, source: &Vec<String>) -> u128 {
+    let start_access = Instant::now();
+    let len = immutable.len();
+    let mut sum: i64 = 0;
+    for i in 0..len {
+        sum += (immutable[i].len() as i64);
+    }
+    let elapsed_access = start_access.elapsed().as_nanos();
+    let mut true_sum: i64 = 0;
+    for i in 0..len {
+        true_sum += (source[i].len() as i64);
+    }
+
+    let correct = (sum == true_sum);
+    println!("{}", correct);
+    return elapsed_access;
+}
+
+fn do_clone<T: Eq + Ord + Clone>(dst: &mut Vec<T>, src: &mut Vec<T>) -> u128 {
+    let start_add = Instant::now();
+    *dst = (*src).clone();
+    start_add.elapsed().as_nanos() 
+}
+
+fn do_clone_from<T: Eq + Ord + Clone>(dst: &mut Vec<T>, src: &mut Vec<T>) -> u128 {
+    let start_add = Instant::now();
+    (*dst).clone_from(src);
+    start_add.elapsed().as_nanos()
 }
 
 unsafe fn select_experiment<T>(method: i32, dst: &mut Vec<T>, src: &mut Vec<T>, size: usize) -> u128 {

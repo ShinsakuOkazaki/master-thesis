856cf3911a6959548828ce0df53ba7c234c57b86
ShinsakuOkazaki <golisaku4639@gmail.com>
2020-04-11 17:02:16 +0000

    d

--- project/practice/run.sh
+++ project/practice/run.sh
@@ -1,18 +1,18 @@
 cargo clean
-cargo build
+cargo build --release
 
 for size in 100 1000 10000 100000
 do
     rm -rf loging.log
     echo "datastructure#k#n_neighbors#threadtime#globaltime" > loging.log
 
-    for method in 1
+    for method in 1 2
     do
         for k in 10000 20000
         do
             for n_neighbors in 10 20 30
             do
-                time cargo run $method $k $n_neighbors ~/master-thesis/data/smallTest/TestingData_"$size".txt  ~/master-thesis/data/trainPartition/*
+                time cargo run --release  $method $k $n_neighbors ~/master-thesis/data/TestingData.txt  ~/master-thesis/data/trainPartition/*
             done
         done
     done

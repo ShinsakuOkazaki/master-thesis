aefb393064be6c274b01f3f3d5f9db43a730c621
ShinsakuOkazaki <golisaku4639@gmail.com>
2020-06-23 06:58:01 +0000

    done

--- project/rust_serialization/run.sh
+++ project/rust_serialization/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-# cargo clean 
-# cargo build --release
+cargo clean 
+cargo build --release
 
-part_file=../data/tables_scale_0.1/part.tbl
-sizes=(100 200 300)
+part_file=../data/tables_scale_0.1/part-large.tbl
+sizes=(900000)
 
-for size_idx in 0 1 2
+for size_idx in 0
 do
     rm -f loging.log
     echo "datastructure#method#size#ser_time#deser_time" > loging.log
@@ -21,9 +21,9 @@ do
             echo "Method: $method, Size: $size, Counter: $counter"
 
             # Server experiment
-            # time taskset -c 0 cargo run --release $part_file $size $method
+            time taskset -c 0 cargo run --release $part_file $size $method
             # Loacal experiment
-            time cargo run --release $part_file $size $method
+            # time cargo run --release $part_file $size $method
         done
     done
     cat loging.log  >  result/resultSerialization_"$size".txt
